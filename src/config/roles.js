950c2f907b2c56dccf3c1e79c37dc6864a46fa70
Perez-salem Tuwan <perezjoshua1811@gmail.com>
2025-12-03 22:01:19 +0100

    initial commit

---
+++ src/config/roles.js
@@ -0,0 +1,13 @@
+const allRoles = {
+  student: [],
+  facilitator: [],
+  admin: ['getUsers', 'manageUsers'],
+};
+
+const roles = Object.keys(allRoles);
+const roleRights = new Map(Object.entries(allRoles));
+
+module.exports = {
+  roles,
+  roleRights,
+};
